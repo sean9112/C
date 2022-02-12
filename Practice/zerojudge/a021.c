cfeb54b2c0e2cf3b51142bf13cd26111b98ff2ca
sean9112 <sean9112@gmail.com>
2022-02-12 16:15:39 +0800

    a021 add while & %c

--- Practice/zerojudge/a021.c
+++ Practice/zerojudge/a021.c
@@ -10,38 +10,38 @@ int main()
     char symbol[2];
     int op1_len, op2_len;
 
-    scanf("%s", op1);
-    scanf("%s", symbol);
-    scanf("%s", op2);
-
-    op1_len = strlen(op1);
-    op2_len = strlen(op2);
-    char op1_tr[op1_len + 1], op2_tr[op2_len + 1];
-
-    for (i = 0; i < op1_len; i++)
+    while (scanf("%s %c %s", op1, &symbol, op2) != EOF)
     {
-        op1_tr[i] = op1[op1_len - 1 - i];
-    }
-    op1_tr[i] = '\0';
-    for (i = 0; i < op2_len; i++)
-    {
-        op2_tr[i] = op2[op2_len - 1 - i];
-    }
-    op2_tr[i] = '\0';
 
-    switch (symbol[0])
-    {
-    case '+':
-        /* code */
-        break;
-    case '-':
-        /* code */
-        break;
-    case '*':
-        /* code */
-        break;
-    case '/':
-        /* code */
-        break;
+        op1_len = strlen(op1);
+        op2_len = strlen(op2);
+        char op1_tr[op1_len + 1], op2_tr[op2_len + 1];
+
+        for (i = 0; i < op1_len; i++)
+        {
+            op1_tr[i] = op1[op1_len - 1 - i];
+        }
+        op1_tr[i] = '\0';
+        for (i = 0; i < op2_len; i++)
+        {
+            op2_tr[i] = op2[op2_len - 1 - i];
+        }
+        op2_tr[i] = '\0';
+
+        // switch (symbol[0])
+        // {
+        // case '+':
+        //     /* code */
+        //     break;
+        // case '-':
+        //     /* code */
+        //     break;
+        // case '*':
+        //     /* code */
+        //     break;
+        // case '/':
+        //     /* code */
+        //     break;
+        // }
     }
 }
